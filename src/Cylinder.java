e2148dba2951d18393f64b9616bf2f4adbbbcbd5
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-06 21:40:08 +0530

    Added Implementation of calculateArea in Cylinder.java

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -7,4 +7,8 @@ class Cylinder extends Shape implements Volume{
         this.radius = radius;
         this.height = height;
     }
+
+    public double calculateArea() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
 }
