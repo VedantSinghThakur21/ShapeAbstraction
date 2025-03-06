d84bcf525e99e02e7c40cead6b76c06829c4ff0d
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-06 21:39:29 +0530

    Defined Cylinder.java

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,2 +1,10 @@
-public class Cylinder {
+class Cylinder extends Shape implements Volume{
+
+    private double radius, height;
+
+    public Cylinder(double radius, double height) {
+        super("Cylinder");
+        this.radius = radius;
+        this.height = height;
+    }
 }
