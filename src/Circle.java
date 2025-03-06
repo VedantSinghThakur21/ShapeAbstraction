803b6ef633c966207b87babea01db529116abc6d
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-06 16:59:54 +0530

    Defined Circle.java and intialised Area n Perimeter Function

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,16 @@
-public class Circle {
-}
+class Circle extends Shape {
+        private double radius;
+
+        public Circle(double radius) {
+            super("Circle");
+            this.radius = radius;
+        }
+
+        public double calculateArea() {
+
+        }
+
+        public double calculatePerimeter() {
+
+        }
+    }
