99dcedd3e162ecd7578903ad1dfcfb43c45ca495
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-06 16:11:42 +0530

    Added Implementation of Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,8 @@
-public class Shape {
+public abstract class Shape {
+    protected String shapeName;
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
 }
