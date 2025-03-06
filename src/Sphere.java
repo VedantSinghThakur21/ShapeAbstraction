38c15bd38437782fa32bd1b8268c380817ae0dc1
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-06 17:10:03 +0530

    Defined sphere.java

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,2 +1,8 @@
-public class Sphere {
+class Sphere extends Shape implements Volume{
+    private double radius;
+
+    public Sphere(double radius) {
+        super("Sphere");
+        this.radius = radius;
+    }
 }
