d2a975f943cd29adbc312748ebdeb04438fb6c28
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-06 17:03:04 +0530

    Defined Rectangle.java and intialised Area n Perimeter Function

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,17 @@
-public class Rectangle {
+class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+
+    }
+
+    public double calculatePerimeter() {
+
+    }
 }
